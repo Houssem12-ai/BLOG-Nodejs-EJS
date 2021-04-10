e892f4518e0c5762d7173ed60a5a1ffdca2a4cc3
Hossprog <hossprog@gmail.com>
2021-04-10 10:40:46 +0100

    editing the article credentials

--- routes/articles.js
+++ routes/articles.js
@@ -2,23 +2,38 @@ const express = require("express");
 const router = express.Router();
 const Article = require("../models/article");
 
+// i can't believe !!!!
+//order matters ?????????????????
 router.get("/new", (req, res) => {
-  res.render("/articles/new");
+  res.render("./articles/new");
 });
-router.get(":id", (req, res) => {
-  res.send("salam");
+
+/* router.get("/new", (req, res) => {
+  const article = {
+    title: "something",
+    description: "des des",
+    markdown: "rod belek",
+  };
+  res.render("./articles/new", { article: article });
+}); */
+
+router.get("/:id", (req, res) => {
+  //res.send("salam");
 });
 
 router.post("/", async (req, res) => {
   const article = new Article({
+    //not making it constant
+    // let cause a problem
     title: req.body.title,
     description: req.body.description,
     markdown: req.body.markdown,
   });
   try {
-    article = await article.save(); //assign a variable to it
-    res.redirect(`/article/${article.id}`);
+    let article = await article.save(); //assign a variable to it
+    res.redirect(`/article/${article.id}`, { article: article });
   } catch (e) {
+    console.log(e);
     res.render("articles/new", { article: article });
   }
 });
--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const app = express();
 const mongoose = require("mongoose");
 const router = require("./routes/articles");
 app.set("view engine", "ejs");
-app.use("/articles", router);
 app.use(express.urlencoded({ extended: false }));
+app.use("/articles", router);
 
-mongoose.connect("mongodb:://localhost/blog", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect("mongodb://localhost:27017/blog", {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("al hamdu leleh"))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   articles = [
     {
@@ -23,7 +26,7 @@ app.get("/", (req, res) => {
       description: "description of the article 2",
     },
   ];
-  res.render("./articles/index", { articles: articles });
+  res.render("./articles/index", { articles: articles }); //it looks by default in views by put ./
 });
 
 app.listen(3003, () => {
