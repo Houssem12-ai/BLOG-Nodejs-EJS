282f62e5f063aa93341094f227457ea9f3267bb3
Hossprog <hossprog@gmail.com>
2021-04-10 11:15:56 +0100

    populating works

--- routes/articles.js
+++ routes/articles.js
@@ -5,7 +5,7 @@ const Article = require("../models/article");
 // i can't believe !!!!
 //order matters ?????????????????
 router.get("/new", (req, res) => {
-  res.render("./articles/new");
+  res.render("./articles/new", { article: new Article() });
 });
 
 /* router.get("/new", (req, res) => {
